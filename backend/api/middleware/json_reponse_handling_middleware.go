9fc4bc75145b0437d9f3584f8b0eccfc801893ea
ilkerciblak <ilkerciblak@gmail.com>
2025-09-22 07:59:25 +0300

    refactor(api/middleware): refactor middleware name

--- backend/api/middleware/json_reponse_handling_middleware.go
+++ backend/api/middleware/json_reponse_handling_middleware.go
@@ -4,24 +4,24 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/ilkerciblak/buldum-app/shared/core/presentation"
+	corepresentation "github.com/ilkerciblak/buldum-app/shared/core/presentation"
 )
 
 type JsonResponseHandlerMiddleware struct {
 }
 
-func (e JsonResponseHandlerMiddleware) Act(endPoint presentation.IEndPoint) http.HandlerFunc {
+func (e JsonResponseHandlerMiddleware) Act(endPoint corepresentation.IEndPoint) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("JsonResponseHandlerMiddleware Calisti")
 		data, err := endPoint.HandleRequest(w, r)
 
 		if err != nil {
-			presentation.RespondWithProblemDetails(w, err)
+			corepresentation.RespondWithProblemDetails(w, err)
 			return
 		}
 
 		if data.Data != nil {
-			presentation.RespondWithJSON(w, data)
+			corepresentation.RespondWithJSON(w, data)
 		}
 		w.WriteHeader(data.StatusCode)
 		log.Printf("JsonResponseHandlerMiddleware Bitirdi")
