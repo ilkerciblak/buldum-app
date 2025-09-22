e6c52e4cb160f4d7e4cb00772020d97af722ace2
ilkerciblak <ilkerciblak@gmail.com>
2025-09-22 09:13:49 +0300

    feat(mock-repo): implement a new mock repository for test issues

---
+++ backend/service/account/internal/infrastructure/repository/mock/mock_account_repository.go
@@ -0,0 +1,48 @@
+package mock
+
+import (
+	"context"
+	"strings"
+
+	"github.com/google/uuid"
+	"github.com/ilkerciblak/buldum-app/service/account/internal/domain/model"
+	"github.com/ilkerciblak/buldum-app/shared/core/coredomain"
+)
+
+// type AccountRepository interface {
+// 	GetById(ctx context.Context, userId uuid.UUID) (*model.Profile, error)
+// 	GetAll(ctx context.Context) ([]*model.Profile, error)
+// 	Create(ctx context.Context, p *model.Profile) error
+// 	Update(ctx context.Context, userId uuid.UUID, p *model.Profile) error
+// 	Delete(ctx context.Context, userId uuid.UUID) error
+// 	Archive(ctx context.Context, userId uuid.UUID) error
+// }
+
+type MockAccountRepository struct {
+}
+
+func (m *MockAccountRepository) GetById(ctx context.Context, userId uuid.UUID) (*model.Profile, error) {
+	return model.NewProfile("ilkerciblak", "url"), nil
+}
+func (m *MockAccountRepository) GetAll(ctx context.Context) ([]*model.Profile, error) {
+	return []*model.Profile{
+		model.NewProfile("ilkerciblak", "url"),
+		model.NewProfile("ilkerciblak", "url"),
+	}, nil
+}
+func (m *MockAccountRepository) Create(ctx context.Context, p *model.Profile) error {
+	if strings.EqualFold("error", p.Username) {
+		return coredomain.BadRequest
+	}
+
+	return nil
+}
+func (m *MockAccountRepository) Update(ctx context.Context, userId uuid.UUID, p *model.Profile) error {
+	return nil
+}
+func (m *MockAccountRepository) Delete(ctx context.Context, userId uuid.UUID) error {
+	return nil
+}
+func (m *MockAccountRepository) Archive(ctx context.Context, userId uuid.UUID) error {
+	return nil
+}
